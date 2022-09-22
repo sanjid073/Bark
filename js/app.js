a755e0d167d6d9b0fb3fcce64444903f73d535cc
sanjid073 <sanjidmahe@gmail.comm>
2022-09-23 01:05:15 +0600

    dfj

--- js/app.js
+++ js/app.js
@@ -5,8 +5,14 @@ function seeItem() {
 $("ul.dropdown-menu .drop-item").on("click", function (event) {
   event.stopPropagation();
 });
-function backToMain(){
+function backToMain() {
   document.getElementById("dropdown-fisrt-item").style.display = "block";
   document.getElementById("hide-dropdown").style.display = "none";
-
+}
+function modalClick() {
+  document.getElementById("modalHeader-img").style.display = "none";
+  document.getElementById("slider-header").innerText = "What is your age?";
+  document.getElementById("first-text").innerText = "Younger then 18";
+  document.getElementById("sec-text").innerText = "18 - 29 years old";
+  document.getElementById("third-text").innerText = "30 - 39 years old";
 }
